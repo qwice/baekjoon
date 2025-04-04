73525c7899bba6bddd0c0ca673723ccf53be7ee8
qwice <134401970+qwice@users.noreply.github.com>
2025-04-04 12:38:35 +0900

    [Silver III] Title: 주유소, Time: 372 ms, Memory: 36044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -4,31 +4,32 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] dist = new long[N - 1];
+        long[] price = new long[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int[] dist = new int[N-1];
-        int[] price = new int[N];
-        
+        for (int i = 0; i < N - 1; i++) {
+            dist[i] = Long.parseLong(st.nextToken());
+        }
+
         st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N - 1 ; i++){
-          dist[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            price[i] = Long.parseLong(st.nextToken());
         }
-        
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i < N ; i++){
-          price[i] = Integer.parseInt(st.nextToken());
+
+        long totalCost = 0;
+        long minPrice = price[0];
+
+        for (int i = 0; i < N - 1; i++) {
+            totalCost += minPrice * dist[i];
+
+            if (price[i + 1] < minPrice) {
+                minPrice = price[i + 1];
+            }
         }
-        
-        int minPrice = price[0];
-        int totalCost = 0;
-        
-        for(int i = 0 ; i < N - 1 ; i++){
-          totalCost += minPrice * dist[i];
-          
-          if(minPrice > price[i+1]) minPrice = price[i+1];
-        }
-        
-        System.out.print(totalCost);
+
+        System.out.println(totalCost);
     }
 }
