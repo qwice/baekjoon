fb710bfb10ba83cbbf3d566375279ccc972fdb8b
qwice <134401970+qwice@users.noreply.github.com>
2025-04-15 16:53:18 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        int[] coins = new int[n];
+        int[] dp = new int[k+1];
+        dp[0] = 1;
+        
+        for(int i = 0 ; i < n ; i++){
+          coins[i] = Integer.parseInt(br.readLine());
+        }
+        
+        for(int coin : coins){
+          for(int i = coin ; i <= k ; i++){
+            dp[i] += dp[i - coin];
+          }
+        }
+        
+        System.out.print(dp[k]);
+    }
+}
