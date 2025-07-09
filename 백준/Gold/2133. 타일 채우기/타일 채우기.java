6d34880fd5f8867494c3be1f3de13beff665dce0
qwice <134401970+qwice@users.noreply.github.com>
2025-07-09 12:12:20 +0900

    [Gold IV] Title: 타일 채우기, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      int N = Integer.parseInt(br.readLine());
+      int[] dp = new int[N+1];
+      
+      for(int i = 1 ; i <= N ; i+=2) {
+        dp[i] = 0;
+      }
+      
+      dp[0] = 1;
+      
+      for(int i = 2 ; i <= N ; i+=2){
+        dp[i] = dp[i-2] * 3;
+        for(int j = i - 4 ; j >= 0 ; j-=2){
+          dp[i] += dp[j] * 2;
+        }
+      }
+      
+      System.out.println(dp[N]);
+    }
+}
