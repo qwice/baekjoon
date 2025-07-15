a3c7c2b85d77ce8e847ae854a245abd2b71d75e4
qwice <134401970+qwice@users.noreply.github.com>
2025-07-15 19:34:28 +0900

    [level 0] Title: ad 제거하기, Time: 0.96 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> result = new ArrayList<>();
+
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+                result.add(str);
+            }
+        }
+
+        return result.toArray(new String[0]);
+    }
+}
