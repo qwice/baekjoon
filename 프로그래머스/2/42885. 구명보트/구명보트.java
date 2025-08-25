f3a69c8136a8da610130c25cf8b8af1c4ac1878c
qwice <134401970+qwice@users.noreply.github.com>
2025-08-25 15:17:24 +0900

    [level 2] Title: 구명보트, Time: 12.77 ms, Memory: 56.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int start = 0;
+        int end = people.length - 1;
+        
+        Arrays.sort(people);
+        
+        while(start <= end) {
+            if(people[start] + people[end--] <= limit) start++;
+            answer++;
+        }
+        return answer;
+    }
+}
