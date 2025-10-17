21d1230f0dfb1d70e496570b61c8d797ac281e02
qwice <134401970+qwice@users.noreply.github.com>
2025-10-17 20:19:18 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1581.43 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+        int answer = 0;
+        
+        for(int i = 1 ; i <= elements.length ; i++){
+            for(int j = 0 ; j < elements.length ; j++) {
+                int sum = 0;
+                for(int k = 0 ; k < i ; k++) {
+                    sum += elements[(j + k) % elements.length];
+                }
+                set.add(sum);
+            }
+        }
+        answer = set.size();
+        
+        return answer;
+    }
+}
