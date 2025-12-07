56115d91e1368ce7feffc9be15011bce20a667a7
qwice <134401970+qwice@users.noreply.github.com>
2025-12-07 19:32:54 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 73.31 ms, Memory: 197 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int n = numbers.length;
+        int[] answer = new int[n];
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            int now = numbers[i];
+
+            while (!stack.isEmpty() && stack.peek() <= now) {
+                stack.pop();
+            }
+
+            answer[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(now);
+        }
+
+        return answer;
+    }
+}
