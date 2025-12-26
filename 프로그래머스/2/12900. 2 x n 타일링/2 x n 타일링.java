b302bbafde3492f97f383cd774b5dbd2e99a6291
qwice <134401970+qwice@users.noreply.github.com>
2025-12-27 02:00:41 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.76 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        final int MOD = 1_000_000_007;
+
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+}
